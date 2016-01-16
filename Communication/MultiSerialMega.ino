a3fe8cd9d844fec29ce984ea6e7db026765aa214
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-01-16 10:39:57 +0900

    MultiSerialMega

---
+++ Communication/MultiSerialMega.ino
@@ -0,0 +1,16 @@
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+}
+
+void loop() {
+  if (Serial1.availabel()) {
+    int inByte = Serial1.read();
+    Serial.write(inByte);
+  }
+
+  if (Serial.available()) {
+    int inByte = Serial.read();
+    Serial1.write(inByte);
+  }
+}
