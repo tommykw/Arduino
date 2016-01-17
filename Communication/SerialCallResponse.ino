1e931d36944f1907484c2fc98738ff44cebf42a5
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-01-17 16:14:29 +0900

    Add SerialCallResponse

---
+++ Communication/SerialCallResponse.ino
@@ -0,0 +1,36 @@
+int firstSensor = 0;
+int secondSensor = 0;
+int thirdSensor = 0;
+int inByte = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    // wait for serial port to connect
+  }
+
+  pinMode(2, INPUT);
+  establishContact();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    inByte = Serial.read();
+    firstSensor = analogRead(A0) / 4;
+    delay(10);
+    secondSensor = analogRead(1) / 4;
+    thirdSensor = map(digitalRead(2), 0, 1, 0, 255);
+    Serial.write(firstSensor);
+    Serial.write(secondSensor);
+    Serial.write(thirdSensor);
+  }
+}
+
+void establishContact() {
+  while (Serial.available() <= 0) {
+    Serial.print('A');
+    delay(300);
+  }
+}
+
+
