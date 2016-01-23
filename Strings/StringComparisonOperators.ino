25e4a4e6da6a32f7732491e093c3e259cb81bcb9
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-01-24 08:53:47 +0900

    [WIP] Add StringComparisonOperator

---
+++ Strings/StringComparisonOperators.ino
@@ -0,0 +1,72 @@
+String one, two;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect
+  }
+
+  one = String("this");
+  two = String("that");
+  Serial.println();
+}
+
+void loop() {
+  if (one == "this") {
+    Serial.println("one == this");
+  }
+
+  if (one != two) {
+    Serial.println("one != two");
+  }
+
+  one = "This";
+  two = "this";
+  if (one != two) {
+    Serial.println(one + " =! " + two);
+  }
+
+  if (one.equals(two)) {
+    Serial.println(one + " equals " + two);
+  } else {
+    Serial.println(one + " does not equal " + two);
+  }
+
+  if (one.equalsIgnoreCase(two)) {
+    Serial.println(one + " equals (ignoring case) " + two);
+  } else {
+    Serial.println(one + " dose not equal (ignoring case) " + two);
+  }
+
+  one = "1";
+  int numberOne = 1;
+  if (one.toINt() == numberOne) {
+    Serial.println(one + " = " + numberOne);
+  }
+
+  one = "2";
+  two = "1";
+  if (one >= two) {
+    Serial.println(one + " >= " + two);
+  }
+
+  one = String("Brown");
+  if (one < "Charles") {
+    Serial.println(one + " < Charles");
+  }
+
+  if (one > "Adams") {
+    Serial.println(one + " > Adams");
+  }
+
+  if (one <= "Browne") {
+    Serial.println(one + " <= Browne");
+  }
+
+  if (one >= "Brow") {
+    Serial.println(one + " >= Brow");
+  }
+
+  one = "Cucumber";
+  two = "Cucuracha";
+}
